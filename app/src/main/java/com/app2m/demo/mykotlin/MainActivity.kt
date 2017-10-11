06e9015d9fb8e41fa8aee04c14cf0d82f50152c5
conghao <hao.cong@qq.com>
2017-10-12 00:52:58 +0800

    init project

---
+++ app/src/main/java/com/app2m/demo/mykotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.app2m.demo.mykotlin
+
+import showToast
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showToast(this, "我是kotlin")
+    }
+}
---
+++ app/src/main/kotlin/KotlinA.kt
@@ -0,0 +1,10 @@
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Created by CongHao on 2017/10/12.
+ * E-mail: hao.cong@app2m.com
+ */
+fun showToast(context: Context, content: String) {
+    Toast.makeText(context, content, Toast.LENGTH_SHORT).show()
+}
